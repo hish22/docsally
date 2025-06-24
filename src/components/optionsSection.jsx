1545227338e6d786f937a3d29e65dca531413d36
Hisham Aljoghaiman <hish522@hotmail.com>
2025-06-24 22:36:06 +0300

    :art: chore: add model logos.

--- src/components/optionsSection.jsx
+++ src/components/optionsSection.jsx
@@ -6,7 +6,9 @@ import devstral from "./../assets/icons/models/devstral.png";
 import qwen3 from "./../assets/icons/models/qwen3.png";
 import gemma3 from "./../assets/icons/models/gemma3.png";
 import llama4 from"./../assets/icons/models/llama4.png";
-import qwen2_5vl from "./../assets/icons/models/qwen2.5vl.png";
+import phi_4 from "./../assets/icons/models/Microsoft_icon.svg.png";
+import mistral from "./../assets/icons/models/mistral-color.svg";
+import olmo from "./../assets/icons/models/olmo.png";
 import createNomicEmbedTextModel from "../util/installNomicEmbedText";
 import { useEffect, useState } from "react";
 
@@ -15,36 +17,45 @@ export default function OptionsSection({setOllama,ollamaList,setSelectedModel}) 
     const [selected,setSelected] = useState("");
 
     const models = [{
-            name: "deepseek-r1",
+            name: "deepseek",
             icon: deepseek_r1
         },{
             name: "devstral",
             icon: devstral
         },{
-            name: "qwen3",
+            name: "qwen",
             icon: qwen3
         },{
-            name: "gemma3",
+            name: "gemma",
             icon: gemma3
         },{
-            name: "llama4",
+            name: "llama",
             icon: llama4
         },{
-            name: "qwen2.5vl",
-            icon: qwen2_5vl
+            name: "phi",
+            icon: phi_4
+        },{
+            name: "mistral",
+            icon: mistral
+        },{
+            name: "qwq",
+            icon: qwen3
+        },{
+            name: "olmo",
+            icon: olmo
         }];
 
         const splitedList = selected.split(":");
 
-        const filteredIcon = models.filter((item) => item.name === splitedList[0].trim());
+        const filteredIcon = models.filter((item) => splitedList[0].trim().includes(item.name));
         
     return (
         <>
             <section id="options-section">
                 <div className="flex">
-                    {filteredIcon.map((item,index) => (
+                    {splitedList[0].trim() ? filteredIcon.map((item,index) => (
                         <img key={index} src={item.icon} width={30}/>
-                    ))}
+                    )): null}
                     <select name="ollamas" id="ollama-select" onChange={e=>{setOllama(e.target.value); setSelected(e.target.value); setSelectedModel(e.target.value)}}>
                     <option value={""}>Select Model</option>
                     {ollamaList.filter((item) => item != "nomic-embed-text:latest").map((item,index) => (
