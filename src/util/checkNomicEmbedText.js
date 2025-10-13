fd0f55fe0feb8608d37e149e4999fbcab8eca54b
Hisham Aljoghaiman <hish522@hotmail.com>
2025-10-13 21:28:03 +0300

    fix: remove nomic-embed-text check phase.

--- src/util/checkNomicEmbedText.js
+++ src/util/checkNomicEmbedText.js
@@ -1,5 +1,5 @@
-import { invoke } from "@tauri-apps/api/core";
+// import { invoke } from "@tauri-apps/api/core";
 
-export default async function checkNomic() {
-    return await invoke("check_nomic");
-}
+// export default async function checkNomic() {
+//     return await invoke("check_nomic");
+// }
--- src/util/installNomicEmbedText.js
+++ src/util/installNomicEmbedText.js
@@ -1,8 +1,8 @@
-import { invoke } from "@tauri-apps/api/core";
+// import { invoke } from "@tauri-apps/api/core";
 
 // Installing nomic-embed-text model
 
-export default async function createNomicEmbedTextModel() {
-    const status = await invoke("install_nemt");
-    return status;
-}
+// export default async function createNomicEmbedTextModel() {
+//     const status = await invoke("install_nemt");
+//     return status;
+// }
