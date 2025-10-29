467a6c295485f60b9e20c6e58aaf92d622b06710
Hisham Aljoghaiman <hish522@hotmail.com>
2025-10-29 15:31:07 +0300

    build: update ollama_rs to 0.3.2 refactor: list ollama models from the ollama_rs api instead of direct stream from the shell/cmd.

--- src-tauri/src/utils/ollama_list.rs
+++ src-tauri/src/utils/ollama_list.rs
@@ -1,23 +1,14 @@
-use std::process::Command;
+use ollama_rs::Ollama;
 
-use crate::utils::cli_type_specification::cmd_type;
 #[tauri::command]
-pub fn ollama_list() -> Vec<String> {
-    let opitions = cmd_type();
-
-    let cmd = Command::new(&opitions[0])
-        .args([&opitions[1], "ollama list"])
-        .output()
-        .expect("failed to execute cmd process");
+pub async fn ollama_list() -> Vec<String> {
+    // Use ollama_rs instead
+    let ollama = Ollama::default();
 
     let mut models: Vec<String> = Vec::new();
-    if cmd.status.success() {
-        let list = String::from_utf8_lossy(&cmd.stdout).to_string();
-        let listvec: Vec<String> = list.split('\n').map(|s| s.to_string()).collect();
-        for i in 1..listvec.len().saturating_sub(1) {
-            let model_name: Vec<&str> = listvec[i].split_whitespace().collect();
-            models.push(model_name.get(0).unwrap().to_string());
-        }
+
+    for model in ollama.list_local_models().await.unwrap() {
+        models.push(model.name);
     }
     models
 }
