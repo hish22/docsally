62dc000e4880d13e44d293ab5dbc3786f007b39c
Hisham Aljoghaiman <hish522@hotmail.com>
2025-11-06 13:36:56 +0300

    refactor: return sql query result of row insertion.

--- src/util/store/insert/insertNewStateChat.js
+++ src/util/store/insert/insertNewStateChat.js
@@ -3,11 +3,7 @@ import Database from "@tauri-apps/plugin-sql"
 export default async function InsertNewStateChat(state_id) {
     const DB = await Database.load('sqlite:state.db');
     
-    DB.execute('INSERT INTO state_chat (state_id) VALUES ($1)',
+    return DB.execute('INSERT INTO state_chat (state_id) VALUES ($1)',
         [state_id]
-    ).then(r => {
-        return r;
-    }).catch(e => {
-        return e;
-    });
+    );
 }
