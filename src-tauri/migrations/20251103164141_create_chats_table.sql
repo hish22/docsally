67d7efccfcc14af4a661161c691e6a7eb42c923e
Hisham Aljoghaiman <hish522@hotmail.com>
2025-11-06 13:30:55 +0300

    refactor: add payload column for chats.

--- src-tauri/migrations/20251103164141_create_chats_table.sql
+++ src-tauri/migrations/20251103164141_create_chats_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS chats(
     chat_id INTEGER PRIMARY KEY,
     user_type TEXT NOT NULL,
+    payload TEXT NOT NULL,
     chat_seq INTEGER NOT NULL,
     state_chat_id INTEGER NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
