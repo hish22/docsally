ef5f877c49078023290cd11a3ab3aacfd264acdc
Hisham Aljoghaiman <hish522@hotmail.com>
2025-11-13 06:50:26 +0300

    feat: copy file from path to /pdf dir.

---
+++ src/util/movePDFFile.js
@@ -0,0 +1,29 @@
+import { copyFile, BaseDirectory, exists, mkdir } from "@tauri-apps/plugin-fs";
+import { appDataDir, join } from "@tauri-apps/api/path";
+import ReplaceSlash from "./slashReplace";
+export default async function MovePDfFile(path) {
+    // Replace (\\ to /) for unique slash type
+    let sp = ReplaceSlash(path);
+
+    const appData = await appDataDir();
+    const desPath = await join(appData,"pdf",sp[sp.length - 1])
+
+    // Check if pdf dir exists
+    const pdfExists = await exists('pdf', {
+        baseDir: BaseDirectory.AppData
+    });
+
+    if (!pdfExists) {
+        await mkdir('pdf', {
+            baseDir: BaseDirectory.AppData
+        });
+    }
+    // move pdf file to pdf dir
+    const pdfFile = await copyFile(path,desPath)
+    .then((f) => {
+        console.log("File moved!");
+    }).catch((e) => {
+        console.log(e);
+    })
+    return await join("pdf",sp[sp.length - 1]);
+}
